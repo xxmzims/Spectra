4c47bec89627b384a7b08010a19563b79c515493
Maksim Ugrinovitch <usmaksim52@gmail.com>
2025-03-13 11:52:53 +0300

    add ParameterizedTest

---
+++ impl/src/test/java/ru/ugrinovich/Spectra/parameterizedTests/AdministratorUtilServiceTest.java
@@ -0,0 +1,30 @@
+package ru.ugrinovich.Spectra.parameterizedTests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.ugrinovich.Spectra.entities.Administrator;
+import ru.ugrinovich.Spectra.services.administrator.AdministratorUtil;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AdministratorUtilServiceTest {
+
+
+    private static Stream<Arguments> provideAdministratorTestArguments(){
+        return Stream.of(Arguments.of(Administrator.builder().name("barsik").build(), Administrator.builder().name("kotik").build()),
+                Arguments.of(Administrator.builder().name(null).build(), Administrator.builder().name("kotik").build()),
+                Arguments.of(Administrator.builder().name("barsik").build(), Administrator.builder().name("").build()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideAdministratorTestArguments")
+    void updateAdministrator_WhenNotNull(Administrator foundAdministrator, Administrator updatedAdministrator){
+        AdministratorUtil.updateDataAdmin(foundAdministrator, updatedAdministrator);
+        assertEquals(foundAdministrator.getName(), updatedAdministrator.getName());
+    }
+}
